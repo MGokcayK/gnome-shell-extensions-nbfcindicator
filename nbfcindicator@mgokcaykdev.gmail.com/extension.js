17e77bb9888aeeb4063f9c05ea6c41cce5873311
MGokcayK <gkcy.kbts@gmail.com>
2020-02-19 16:37:35 +0300

    First upload

---
+++ nbfcindicator@mgokcaykdev.gmail.com/extension.js
@@ -0,0 +1,361 @@
+/*
+ * Copyright (C) 2020 Mehmet Gökçay Kabataş - mgokcayk <mgokcaykdev@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+ // imports 
+const St = imports.gi.St;
+const Clutter = imports.gi.Clutter;
+const Main = imports.ui.main;
+const PanelMenu = imports.ui.panelMenu;
+const PopupMenu = imports.ui.popupMenu;
+const Lang = imports.lang;
+const Mainloop = imports.mainloop;
+const Slider = imports.ui.slider;
+const GLib = imports.gi.GLib;
+const Util = imports.misc.util;
+
+const Me = imports.misc.extensionUtils.getCurrentExtension();
+const Convenience = Me.imports.convenience;
+
+const Nbfc_Indicator = new Lang.Class({
+	Name: 'Nbfc_Indicator',	// Class Name
+	Extends: PanelMenu.Button,	// Parent Class
+
+	// Constructor
+	_init: function() {
+		this.parent(0, 'Nbfc_Indicator', false);
+
+		let box = new St.BoxLayout(); // create box 
+		
+		// create icon
+		var theme = imports.gi.Gtk.IconTheme.get_default();
+		let icon_dir = Me.dir.get_child('icons');
+		theme.append_search_path(icon_dir.get_path());
+		icon = new St.Icon({ icon_name: 'nbfc-icon',
+		                         style_class: 'system-status-icon'});
+
+		// create label for panel
+		toplabel = new St.Label({ text: ' Nbfc ', 
+			y_expand: true,
+			y_align: Clutter.ActorAlign.CENTER });
+
+		box.add(icon); // add icon to box
+		box.add(toplabel); // add label to box
+
+		this.actor.add_child(box); // add box to panel 
+
+		this.settings = Convenience.getSettings(); // get settings from schemas
+
+		// creatings menu items
+		serviceEnable = new PopupMenu.PopupSwitchMenuItem('Service Enable ');
+		readOnly = new PopupMenu.PopupMenuItem('Read Only ',{reactive:false});
+		selectedConfigName = new PopupMenu.PopupMenuItem('Selected Config ', { reactive: false });
+		temperature = new PopupMenu.PopupMenuItem('Temperature ' , { reactive: false });
+		fanName = new PopupMenu.PopupMenuItem('Fan Display Name ', { reactive: false });
+		autoControl = new PopupMenu.PopupSwitchMenuItem('Auto Control ');
+		critMode = new PopupMenu.PopupMenuItem('Critical Mode ', {reactive: false});
+		fanSpeed = new PopupMenu.PopupMenuItem('Fan Speed ', { reactive: false });
+		tarFanSpeed = new PopupMenu.PopupMenuItem('Fan Target Speed', { reactive: false });
+		slider = new Slider.Slider(0.5);
+		sliderContainer = new PopupMenu.PopupBaseMenuItem();
+		sliderContainer.actor = slider.actor;
+
+		//debuggerG = new PopupMenu.PopupMenuItem('Debugger ', { reactive: false }); 
+		
+		// add menu items to menu
+		this.menu.addMenuItem(serviceEnable);
+		this.menu.addMenuItem(readOnly);
+		this.menu.addMenuItem(selectedConfigName);
+		this.menu.addMenuItem(temperature);
+		this.menu.addMenuItem(autoControl);
+		this.menu.addMenuItem(fanName);
+		this.menu.addMenuItem(critMode);
+		this.menu.addMenuItem(fanSpeed);
+		this.menu.addMenuItem(tarFanSpeed);
+		this.menu.addMenuItem(sliderContainer);
+		//this.menu.addMenuItem(debuggerG);
+
+		// adding settings
+		this._addSettings();
+
+		// handle switch and slider events with connect
+		serviceEnable.connect('toggled', Lang.bind(this, function(object, value){
+			if (value)
+			{
+				GLib.spawn_command_line_sync('mono /opt/nbfc/nbfc.exe start -e');
+				this._updateDisplay();
+			}else
+			{
+				GLib.spawn_command_line_sync('mono /opt/nbfc/nbfc.exe stop');
+				this._updateDisplay();
+			}
+		}));
+
+		autoControl.connect('toggled', Lang.bind(this, function(object, value){
+			if (value)
+			{
+				GLib.spawn_command_line_sync('mono /opt/nbfc/nbfc.exe set -a');
+				this._updateDisplay();
+			}else
+			{
+				let cmd = 'mono /opt/nbfc/nbfc.exe set -s' + String(fanSpeedValue);
+				GLib.spawn_command_line_sync(cmd);
+				this._updateDisplay();
+			}
+		}));
+
+		slider.connect('drag-end', (_slider) => this._onSliderValueChanged(_slider));
+
+		// create connection refuse counter
+		connectionRefusedCounter = 0;
+
+		// create update time previouse variable to check it is changed or not/
+		updateTimePre = this.settings.get_int('update-time');
+
+		//update display
+		this._updateDisplay();
+
+		// adding timer
+		this._addTimer();		
+ 	 },
+
+
+	_updateDisplay : function()
+	{	
+		// check whether update timer changed from settings or not 
+		if (updateTimePre != this.settings.get_int('update-time'))
+		{
+			updateTimePre = this.settings.get_int('update-time');
+			this._updateTimeChanged();
+		}
+		
+		// run command line to get program output
+		commandLineOutput = GLib.spawn_command_line_sync('mono /opt/nbfc/nbfc.exe status -a').toString();
+		var checker = commandLineOutput.split(",");
+		var line = commandLineOutput.split("\n");
+
+		// check whether connection refused or not from command line output
+		if (checker[2].length < 20 && checker[2].length > 10)
+		{
+			connectionRefused = new PopupMenu.PopupMenuItem("Connection Refused..", {reactive:false});
+			
+			var connectMessage = new PopupMenu.PopupMenuItem("Run : systemctl enable nbfc --now",{reactive:false});
+			
+			// if connection refused add some menu items to warn user.
+			if (connectionRefusedCounter < 1)
+			{
+				sep = new PopupMenu.PopupSeparatorMenuItem();
+				this.menu.addMenuItem(sep);
+				this.menu.addMenuItem(connectionRefused);
+				this.menu.addMenuItem(connectMessage);
+				fanSpeed.label.text = "Fan Speed : undefined";
+				tarFanSpeed.label.text ="Fan Target Speed : undefined ";
+				slider.value = 0.0;
+				connectionRefusedCounter += 1;
+				this._updateDisplay();
+			}			
+			return;
+		}else
+		{
+			// if program become enable from connection refuse conditon, change some properties.
+			if (connectionRefusedCounter == 1)
+			{
+				connectionRefusedCounter = 0;
+				this._updateDisplay(); 
+			}else // if program not in connection refused but there are some menu children, remove it.
+			{
+				let children = this.menu._getMenuItems();
+				if (children.length > 11)
+				{
+					for(let i=children.length; i>11; i--)
+					{
+						children[children.length-1].destroy();
+					}				
+					this._updateDisplay();
+				}			
+			}
+		}		
+		
+		// service status value and change switch w.r.t output
+		var valServStat = line[0].split(": ")[1];
+		if (valServStat == "True")
+		{
+			serviceEnable.setToggleState(1);
+		}else
+		{
+			serviceEnable.setToggleState(0);
+			slider.setValue(0.0);
+			toplabel.text = 'Nbfc - Service Disable';
+		}
+
+		/* READ ONLY PROPERTIES NOT WORK IN TERMINAL SO DISABLED. JUST ADDED TEXT
+		var valReadOnly = line[1].split(": ")[1];
+		
+		readOnly.connect('toggle',Lang.bind(this, function(object, value)
+		{	
+			GLib.spawn_command_line_sync('mono /opt/nbfc/nbfc.exe stop');
+			if (value) 
+			{
+				
+				GLib.spawn_command_line_sync('mono /opt/nbfc/nbfc.exe start -r');
+				this._updateDisplay();
+			}else 
+			{
+				GLib.spawn_command_line_sync('mono /opt/nbfc/nbfc.exe start -e');
+				this._updateDisplay();
+			}
+		}));
+		if (valReadOnly == "True")
+		{
+			readOnly.setToggleState(1);
+		}else
+		{
+			readOnly.setToggleState(0);
+		}
+		*/
+		readOnly.label.text = 'Read Only : not work proper in terminal';
+
+		// check config file names from setting page. if different apply config, else do nothing.
+		strCnfg = String(line[2].split(": ")[1]);
+		schemaCnfg = this.settings.get_string('configs').split(".")[0];
+		if (strCnfg != schemaCnfg)
+		{
+			cmd = 'mono ' + this.settings.get_string('location').split('/Config')[0] + "/nbfc.exe config --apply '" + schemaCnfg + "'";
+			GLib.spawn_command_line_sync(cmd);
+			txtCnfg = schemaCnfg;
+		}else 
+		{
+			txtCnfg = strCnfg;
+		}
+
+		// edit menu items 
+		var txtSelConFile = "Selected Config File : " + txtCnfg ;
+		selectedConfigName.label.text = txtSelConFile;
+
+		var txtTemp = "Temperature : " + String(line[3].split(": ")[1]);
+		temperature.label.text = txtTemp;
+
+		var txtFanName = "Fan Display Name : " + String(line[5].split(": ")[1]);
+		fanName.label.text = txtFanName;
+
+		var valAutoControl = line[6].split(": ")[1];
+		if (valAutoControl == "True")
+		{
+			autoControl.setToggleState(1);
+		}else
+		{
+			autoControl.setToggleState(0);
+		}
+
+		// if service is enable, get fan status.		
+		if(line[7])
+		{
+			var valCritMode = line[7].split(": ")[1];
+			if (valCritMode == "True")
+			{
+				critMode.label.text = 'Critic Mode : True. Cannot change by user.';
+			}else
+			{
+				critMode.label.text = "Critic Mode : False. Cannot change by user.";
+			}
+
+			fanSpeedValue = line[8].split(": ")[1];
+			var txtFanSpeed = "Fan Speed : " + String(fanSpeedValue) ;
+			fanSpeed.label.text = txtFanSpeed;
+			toplabel.text = "FS: "+txtFanSpeed.split(":")[1];
+					
+			var txtTarFanSpeed = "Fan Target Speed : " + String(line[9].split(": ")[1]) ;
+			tarFanSpeed.label.text = txtTarFanSpeed;
+
+			slider.setValue(fanSpeedValue / 100);
+		}
+
+		//debuggerG.label.text = String(this.settings.get_string('configs'));
+
+	},
+
+	// run command w.r.t slider value.		
+	_onSliderValueChanged(_slider) {
+		if (autoControl.state == 0)
+		{
+			let cmd = 'mono /opt/nbfc/nbfc.exe set -s' + String(slider.value * 100);
+			GLib.spawn_command_line_sync(cmd);
+			this._updateDisplay();
+		}
+	},
+
+	// update time when it is changed.
+	_updateTimeChanged : function(){
+        Mainloop.source_remove(this._timeoutId);
+        this._addTimer();
+    },
+
+	// adding timer
+	_addTimer : function(){
+        this._timeoutId = Mainloop.timeout_add_seconds(this.settings.get_int('update-time'), Lang.bind(this, function (){
+            this._updateDisplay();
+            return true;
+        }));
+	},
+	
+	_addSettings : function(){
+        // add separator
+        this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
+
+		// create settings
+        let settings = new PopupMenu.PopupBaseMenuItem();
+        settings.actor.add(new St.Label({ text: 'Settings' }), { expand: true, x_fill: false });
+
+		// handle signals
+        settings.connect('activate', function () {
+            Util.spawn(["gnome-shell-extension-prefs", Me.metadata.uuid]);
+        });
+
+		// add settings
+        this.menu.addMenuItem(settings);
+    },
+
+	// destroy the class  
+	destroy: function() {
+		this.parent();
+	}
+});
+
+
+// Global variables for use as button to click 
+let button;
+
+// initialize function
+function init() {}
+
+// main function
+function enable() {
+	// create button
+	button = new Nbfc_Indicator;
+
+	// add button to panel
+	Main.panel.addToStatusArea('PopupMenuExample', button, 0, 'right');
+}
+
+// destructor of main function
+function disable() {
+	button.destroy();	
+}
---
+++ nbfcindicator@mgokcaykdev.gmail.com/prefs.js
@@ -0,0 +1,174 @@
+/*
+ * Copyright (C) 2020 Mehmet Gökçay Kabataş - mgokcayk <mgokcaykdev@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+const GObject = imports.gi.GObject;
+const Gio = imports.gi.Gio;
+const Gtk = imports.gi.Gtk;
+const Lang = imports.lang;
+const GLib = imports.gi.GLib;
+
+const Me = imports.misc.extensionUtils.getCurrentExtension();
+const Convenience = Me.imports.convenience;
+const Gettext = imports.gettext.domain(Me.metadata['gettext-domain']);
+const _ = Gettext.gettext;
+
+const NbfcPrefsWidget = new GObject.Class({
+    Name: 'Nbfc.Prefs.Widget',
+    GTypeName: 'NbfcPrefsWidget',
+    Extends: Gtk.Grid,
+
+    _init: function(params) {
+        this.parent(params);
+        this.margin = this.row_spacing = this.column_spacing = 20;
+
+        this.settings = Convenience.getSettings(); // get settings files from schemas 
+
+        this._addSpinButton({label : 'Update Time (sec) : ', x: 0, y: 0}); // add spin button
+
+        this._getConfigList(); 
+
+        this._addEntry({label: "Config location (current : " + this.settings.get_string('location') + ") :", x : 0, y : 1, key: 'location'}); // add entry for location
+        
+        this.status = this._addLabel({label : '', x:0, y: 3}); // add status 
+
+        this._addComboBox({label: " Config : " + this.settings.get_string('configs'), x :0, y : 2}); // add combobox for config
+ 
+        
+        
+    },
+
+    _getConfigList : function() 
+    {
+        items = ["..."]; // items in location file
+        
+        cmd = "ls " +this.settings.get_string('location'); // command for listing files in 'location' file.
+
+        let fileExist = GLib.file_test("Desktop",GLib.FileTest.IS_DIR); // check there is file named as 'location'.
+        if (!fileExist)
+        {
+            cmd = "ls"; // make base file as /home/USER.
+            this.status.label = 'Error : Location not found. '
+        }   
+        if(GLib.spawn_command_line_sync(cmd)[0]) // check command has not error.
+        {
+            configs = GLib.spawn_command_line_sync(cmd)[1].toString(); // apply cmd 
+        }else
+        {
+            this.status.label = 'Error : ' + cmd + ' input has error.';
+        }
+        
+        configs = configs.split("\n"); // split configs data w.r.t lines
+
+        for (let it =0; it<configs.length; it++)
+            if(configs[it].split(".")[1] == 'xml')
+            {
+                items.push(configs[it]); // add configs name to items 
+            }
+            
+
+        //this.settings.set_string('configs', configs[0]); // change 'configs' in schemas as first item of items.
+
+        if(this._comboBoxText)
+        {
+            this._editComboBox(); // if there is combobox created, edit it.
+        }        
+
+    },
+
+    _editComboBox : function()
+    {
+        this._comboBoxText.remove_all(); // remove all items in combobox
+        
+        for (let i = 0; i < items.length; i++) 
+            this._comboBoxText.append_text (items[i]); // add all items in items.
+    },
+
+    _addLabel : function(params)
+    {
+        let lbl = new Gtk.Label({label:params.label, halign : Gtk.Align.START});
+        this.attach(lbl, params.x, params.y, 1, 1);
+        return lbl;
+    },
+
+    _addSpinButton : function(params)
+    {
+        this._addLabel({label : params.label, x : params.x, y: params.y});
+        let updateTime = Gtk.SpinButton.new_with_range (1, 60, 1);
+        this.attach(updateTime, params.x + 1, params.y, 1, 1);
+        this.settings.bind('update-time', updateTime, 'value', Gio.SettingsBindFlags.DEFAULT);
+    },
+
+    _addEntry : function(params)
+    {
+        let lbl = this._addLabel(params);
+        let entry = new Gtk.Entry({halign : Gtk.Align.START, valign : Gtk.Align.CENTER})
+        this.attach(entry, params.x + 1, params.y, 1, 1);
+        entry.connect('activate',Lang.bind(this, function() {
+            this.settings.set_string(params.key, entry.get_text())
+            let cloc = "Location (current : " + entry.get_text() + ") :";
+            lbl.label = cloc;
+            this._getConfigList();
+        }));
+    },
+
+    _addComboBox : function(params)
+    {
+        let lbl = this._addLabel(params);
+        this._comboBoxText = new Gtk.ComboBoxText(); // create combobox
+
+        this._editComboBox(); 
+
+        this._comboBoxText.set_active (0); // active element is 0
+        this.attach(this._comboBoxText,params.x + 1,params.y,1,1);
+
+        // if combobox changed, get current item and edit to schema
+
+        this._comboBoxText.connect('changed', Lang.bind(this, function(){            
+            let active = this._comboBoxText.get_active();
+            this.settings.set_string('configs', String(configs[active-1]));
+            lbl.label = " Config : " + this.settings.get_string('configs');
+        }));
+    },
+
+    _addSwitch : function(params)
+    {
+        let lbl = new Gtk.Label({label: params.label,halign : Gtk.Align.END});
+        this.attach(lbl, params.x, params.y, 1, 1);
+        let sw = new Gtk.Switch({halign : Gtk.Align.END, valign : Gtk.Align.CENTER});
+        this.attach(sw, params.x + 1, params.y, 1, 1);
+        if(params.help){
+            lbl.set_tooltip_text(params.help);
+            sw.set_tooltip_text(params.help);
+        }
+        this.settings.bind(params.key, sw, 'active', Gio.SettingsBindFlags.DEFAULT);
+    },
+
+});
+
+function init() {
+    Convenience.initTranslations();
+}
+
+function buildPrefsWidget() {
+    let widget = new NbfcPrefsWidget();
+    widget.show_all();
+    return widget;
+}
